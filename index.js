d819e493df3a03f37c4697786db556ddcac832ef
mickey english <mickeyeng92@gmail.com>
2020-02-25 17:35:43 +0000

    dynamically show users score

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const correctAnswers = ['B', 'B', 'B', 'B'];
 const form = document.querySelector('.quiz-form');
+const result = document.querySelector('.result');
 
 form.addEventListener('submit', e => {
   e.preventDefault();
@@ -20,5 +21,6 @@ form.addEventListener('submit', e => {
     }
   });
 
-  console.log(score);
+  result.querySelector('span').textContent = `${score}%`;
+  result.classList.remove('d-none');
 });
